d472e66e877722594104381d8757f01a49d347df
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2023-05-16 15:32:02 -0700

    change logo
    Pushed from Domino: https://rev4demo12597.product-team-sandbox.domino.tech/workspace/integration-test/Rev4_Chapter_1?executionId=6459602bf2ad0d303f6fbc72

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ with container:
     if st.session_state['generated']:
         with response_container:
             for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-                message(st.session_state["generated"][i], key=str(i))
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', logo='user.png')
+                message(st.session_state["generated"][i], key=str(i),logo='domino.png')
                 if 'total_tokens' in st.session_state and len(st.session_state['total_tokens']) > 0:
                     st.write(f"Number of tokens: {st.session_state['total_tokens'][i]}")
